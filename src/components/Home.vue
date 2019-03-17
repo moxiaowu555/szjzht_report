6c91f14679b6f14ba43ab5e41dbba52057cf3619
xiaowu <550200360@qq.com>
2019-03-17 22:31:06 +0800

    初始化项目

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{msg}}</h1>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: '欢迎来到首页'
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
